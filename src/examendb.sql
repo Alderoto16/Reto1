3d41ab0692830d044baef59739c2ca83813999ba
Alder <aldersanchez163@gmail.com>
2024-09-12 12:13:22 +0200

    arreglos

---
+++ src/examendb.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS examendb;
+USE examendb;
+
+CREATE TABLE IF NOT EXISTS UnidadDidactica (
+    id INTEGER PRIMARY KEY,
+    acronimo VARCHAR(50),
+    titulo VARCHAR(100),
+    evaluacion VARCHAR(50),
+    descripcion VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS Enunciado (
+    id INTEGER PRIMARY KEY,
+    descripcion TEXT NOT NULL,
+	nivel ENUM('ALTA', 'MEDIA', 'BAJA'),
+    disponible BOOLEAN DEFAULT TRUE,
+    ruta VARCHAR(255),
+    convocatoria_examen VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS ConvocatoriaExamen (
+    convocatoria VARCHAR(50) PRIMARY KEY,
+    descripcion VARCHAR(100),
+    fecha DATE NOT NULL,
+    curso VARCHAR(100),
+    id INTEGER,
+    FOREIGN KEY (id) REFERENCES Enunciado(id)
+);
+
+CREATE TABLE IF NOT EXISTS UnidadDidactica_Enunciado (
+    unidad_id INTEGER,
+    enunciado_id INTEGER,
+    PRIMARY KEY (unidad_id, enunciado_id),
+    FOREIGN KEY (unidad_id) REFERENCES UnidadDidactica(id),
+    FOREIGN KEY (enunciado_id) REFERENCES Enunciado(id)
+);
