c44b35d12b77e20b385db9415dbaea3b7760a530
Omar <multitartanga@gmail.com>
2024-09-12 13:17:03 +0200

    el menu y nombre de las clases

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -17,4 +17,42 @@ public class Controller implements IController {
 	private PreparedStatement statement;
 	private ResultSet resultSet;
 	private CallableStatement callableStatement = null;
+
+    @Override
+    public void crearUnidad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crearConvocatoria() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crearEnunciado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void consultarEnunciadosPorUnidad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void consultarConvocatoriasConEnunciado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void visualizarTextoEnunciado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void asignarEnunciadoConvocatoria() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+        
+        
 }
